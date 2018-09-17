c5fa759f4417506d07f44a69d65feb1b1d9275ef
NickitaSolovei <43146250+NickitaSolovei@users.noreply.github.com>
2018-09-17 14:37:43 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var answer1 = 0;
+
+  function divide5(n){
+    var newN = n / 5;
+    if (newN >= 1){
+      newN = Math.floor(newN);
+      answer1 += newN;
+      divide5(newN);
+    }
+  }
+  divide5(number);
+
+  return answer1;
 }
